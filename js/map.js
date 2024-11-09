873959cde36cd124038e977824a4b13135963810
Kylo P <100837335+cywf@users.noreply.github.com>
2024-11-09 14:14:48 +0000

    Added workflow

--- js/map.js
+++ js/map.js
@@ -3,11 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
     var map = L.map("map").setView([18.2208, -66.5901], 8);
 
     // Add the Matrix tileset
-    L.tileLayer("https://tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token=YOUR_ACCESS_TOKEN", {
+    fetch('/data/config.json')
+    .then((response) => response.json())
+    .then((config) => {
+      const map = L.map('map').setView([18.2208, -66.5901], 8);
+      L.tileLayer(`https://tile.jawg.io/jawg-matrix/{z}/{x}/{y}.png?access-token=${config.MAPBOX_ACCESS_TOKEN}`, {
         attribution: '<a href="https://jawg.io" target="_blank">© Jawg</a>',
         minZoom: 0,
         maxZoom: 22,
-    }).addTo(map);
+      }).addTo(map);
+    })
+    .catch((error) => console.error('Error loading config:', error));
 
     // Load markers from the JSON file
     fetch("data/markers.json")
